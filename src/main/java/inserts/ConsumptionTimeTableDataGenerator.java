2ac747bf35820bad3a92abeb7b858c8bff334376
DamianTujdowski <d.tujdowski@gmail.com>
2020-10-22 00:05:27 +0200

    Added generating random consumption date and time

--- src/main/java/inserts/ConsumptionTimeTableDataGenerator.java
+++ src/main/java/inserts/ConsumptionTimeTableDataGenerator.java
@@ -1,19 +1,38 @@
 package inserts;
 
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class ConsumptionTimeTableDataGenerator extends DataGenerator {
+public class ConsumptionTimeTableDataGenerator {
 
 
-    String generateInsertIntoQueries(String rowsNumber) {
+    public String generateInsertIntoQueries(String rowsNumber, String mealId) {
         return IntStream.rangeClosed(0, Integer.parseInt(rowsNumber))
-                .mapToObj(this::mapToSingleInsertIntoQuery)
+                .mapToObj(num -> mapToSingleInsertIntoQuery(num, mealId))
                 .collect(Collectors.joining("\n"));
     }
 
 
-    String mapToSingleInsertIntoQuery(int id) {
-        return null;
+    private String mapToSingleInsertIntoQuery(int id, String mealId) {
+        return String.format("INSERT INTO consumption_time (id, consumption_time, meal_id) VALUES (%d, %s, %s)",
+                id, generateDateTime(), mealId);
+    }
+
+    private String generateDateTime() {
+        return String.format("\'%s %s\'", generateDate(), generateTime());
+    }
+
+    private String generateDate() {
+        return String.format("2020-%s-%s", generateRandomDateOrTime(12), generateRandomDateOrTime(30));
+    }
+
+    private String generateTime() {
+        return String.format("%s:%s:00", generateRandomDateOrTime(12), generateRandomDateOrTime(60));
+    }
+
+    private String generateRandomDateOrTime(int bound) {
+        int dateOrTime = new Random().nextInt(bound);
+        return dateOrTime < 10 ? "0" + dateOrTime : "" + dateOrTime;
     }
 }
